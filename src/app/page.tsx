7d466d15167c5303e4c3c37584cd614cc36d6c20
niiko <nicolas.rocagel@gmail.com>
2023-11-08 17:45:34 +0100

    feat: add connection to db through Prisma

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@nextui-org/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>brad</h1>
+      <Button>Click me</Button>
+    </main>
+  );
+}
