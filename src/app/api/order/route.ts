6b48900bdd26b04a8a7c9bb7fe39c38b5da8dccb
niiko <nicolas.rocagel@gmail.com>
2023-11-14 10:38:08 +0100

    feat: finish update inventory

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: NextRequest) {
   const items: any = await req.json();
 
   try {
-    const order = await prisma.orders.create({
+    await prisma.orders.create({
       data: {
         user_id: 1,
         OrderItems: {
@@ -21,9 +21,52 @@ export async function POST(req: NextRequest) {
         },
       },
     });
-    console.log(order);
 
-    return NextResponse.json({ data: order }, { status: 201 });
+    const inventoryUpdate = await items.map((item: any) => {
+      console.log("💥");
+      console.log(item);
+
+      const product = prisma.products
+        .findUnique({
+          where: {
+            id: item.product.id,
+          },
+          select: {
+            inventory: true,
+          },
+        })
+        .then((res) => {
+          if (item.quantity > res?.inventory!) {
+            return NextResponse.json(
+              { response: "Not enough items in inventory left" },
+              { status: 500 }
+            );
+          } else {
+            return res;
+          }
+        })
+        .then((res) => {
+          console.log("in RES 2 🚀");
+
+          console.log(res);
+
+          prisma.products
+            .update({
+              where: {
+                id: item.product.id,
+              },
+              data: {
+                inventory: {
+                  decrement: item.quantity,
+                },
+              },
+            })
+            .then((updateRes) => console.log({ updateRes }));
+        })
+        .catch((err) => NextResponse.json({ err }, { status: 500 }));
+    });
+
+    return NextResponse.json({ data: "order placed" }, { status: 201 });
   } catch (err) {
     console.log(err);
 
